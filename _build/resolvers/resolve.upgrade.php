4f034909d08b59352d4ae259e2debf1774f89cc8
sergant210 <sergant210@bk.ru>
2019-01-15 20:04:53 +0300

    v.2.2.2-pl

--- _build/resolvers/resolve.upgrade.php
+++ _build/resolvers/resolve.upgrade.php
@@ -10,16 +10,6 @@ if ($transport->xpdo) {
             break;
 
         case xPDOTransport::ACTION_UPGRADE:
-            if (!empty($options['chunks']) && !empty($options['update_chunks'])) {
-                foreach ($options['update_chunks'] as $v) {
-                    if (!empty($options['chunks'][$v]) && $chunk = $modx->getObject('modChunk', array('name' => $v))) {
-                        $chunk->set('snippet', $options['chunks'][$v]);
-                        $chunk->save();
-                        $modx->log(modX::LOG_LEVEL_INFO, 'Updated chunk "<b>' . $v . '</b>"');
-                    }
-                }
-            }
-
             /** @var modAction $action */
             if ($action = $modx->getObject('modAction', array('namespace' => 'sitestatistics'))) {
                 $action->remove();
